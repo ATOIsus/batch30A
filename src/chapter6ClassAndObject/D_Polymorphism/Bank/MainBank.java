8e13232b21547e6c3b4c8a79ba8267cdcc8e4c15
ATOIsus <sabinmaharjan884@gmail.com>
2022-01-02 11:59:46 +0545

    Example of Polymorphism done.

--- src/chapter6ClassAndObject/D_Polymorphism/Bank/MainBank.java
+++ src/chapter6ClassAndObject/D_Polymorphism/Bank/MainBank.java
@@ -3,8 +3,10 @@ package chapter6ClassAndObject.D_Polymorphism.Bank;
 public class MainBank {
 
 
+    private int InterestRate = 10;
+
     protected int getInterestRate(){
-        return 10;
+        return InterestRate;
     }
 
 
